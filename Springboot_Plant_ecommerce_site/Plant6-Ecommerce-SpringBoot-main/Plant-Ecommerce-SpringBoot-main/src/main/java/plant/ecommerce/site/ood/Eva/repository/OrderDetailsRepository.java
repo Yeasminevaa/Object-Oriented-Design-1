6c3574ae46009f7c3397209725ea499b975e48e4
YeasMin Akter Eva <eva2305341240@diu.edu.bd>
2025-04-18 19:56:45 +0600

    Update OrderDetailsRepository.java

--- Springboot_Plant_ecommerce_site/Plant6-Ecommerce-SpringBoot-main/Plant-Ecommerce-SpringBoot-main/src/main/java/plant/ecommerce/site/ood/Eva/repository/OrderDetailsRepository.java
+++ Springboot_Plant_ecommerce_site/Plant6-Ecommerce-SpringBoot-main/Plant-Ecommerce-SpringBoot-main/src/main/java/plant/ecommerce/site/ood/Eva/repository/OrderDetailsRepository.java
@@ -1,6 +1,6 @@
-package plant.ecommerce.site.ood.zerin.repository;
+package plant.ecommerce.site.ood.eva.repository;
 
-import plant.ecommerce.site.ood.zerin.model.OrderDetails;
+import plant.ecommerce.site.ood.eva.model.OrderDetails;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
